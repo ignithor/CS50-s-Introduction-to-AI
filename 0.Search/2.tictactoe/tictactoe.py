e7e4aa703da4075b385afbe7bd39037fb43bbce8
Paul P <paul.phamdang@gmail.com>
2025-10-31 15:52:17 +0900

    HMI working with random play

--- 0.Search/2.tictactoe/tictactoe.py
+++ 0.Search/2.tictactoe/tictactoe.py
@@ -50,32 +50,60 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
-
+    available = actions(board)
+    if not(action in available):
+        raise ValueError("Action not allowed")
+    turn = player(board)
+    new_board = board.copy()
+    new_board[action[0]][action[1]]=turn
+    return(new_board)
+    
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in range(3):
+        if board[i][0]==board[i][1]==board[i][2]!=EMPTY:
+            return(board[i][0])
+        if board[0][i]==board[1][i]==board[2][i]!=EMPTY:
+            return(board[0][i])
+    if board[0][0]==board[1][1]==board[2][2]!=EMPTY:
+        return(board[0][0])
+    if board[0][2]==board[1][1]==board[2][0]!=EMPTY:
+        return(board[0][2])
+    return(None)
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    finish = winner(board)
+    if finish != None or actions(board)==[]:
+        return(True)
+    else:
+        return(False)
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
-
+    end = winner(board)
+    if end == None:
+        return(0)
+    elif end==X:
+        return(1)
+    else:
+        return(-1)
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    move= actions(board)
+    if move==[]:
+        return(None)
+    else:
+        return(move[0])
